be6016cc3babeeb9a896827cfb5dee9e58b7c874
nadinnah <nadinahmedmohamed@gmail.com>
2026-01-07 15:32:22 +0300

    add designTacoController

--- taco-cloud-application/taco-cloud/src/main/java/com/example/tacocloud/models/Ingredient.java
+++ taco-cloud-application/taco-cloud/src/main/java/com/example/tacocloud/models/Ingredient.java
@@ -1,9 +1,11 @@
 package com.example.tacocloud.models;
 
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
-@Data
-public class Ingredients {
+@Data //tells Lombok to generate all of missing methods as well as a constructor that accepts all final properties as arguments.
+@RequiredArgsConstructor
+public class Ingredient {
 
     private final String id; //final variable cant be reassigned once it's been initialized
     private final String name;
