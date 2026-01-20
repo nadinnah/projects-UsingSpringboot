a3fc3d279c89002cd8d8949589fd94f070579df0
nadinnah <nadinahmedmohamed@gmail.com>
2026-01-20 13:00:57 +0300

    merge to main

--- taco-cloud-application/taco-cloud/src/main/java/com/example/tacocloud/TacoCloudApplication.java
+++ taco-cloud-application/taco-cloud/src/main/java/com/example/tacocloud/TacoCloudApplication.java
@@ -22,3 +22,14 @@ public class TacoCloudApplication {
 //@Component → class is a bean
 //@Bean → method returns a bean
 //@Autowired → inject bean from context
+
+//Application Context is like a factory and registry for your objects (aka beans).
+//it is Spring’s brain for managing your app components
+
+//A bean is just an object that Spring creates, manages, and knows about.
+//like any normal Java object (new MyClass()), but Spring controls its lifecycle, wiring, and availability for your whole application
+
+//Spring uses annotations to register classes or methods as beans in the context
+//@Component → class is a bean
+//@Bean → method returns a bean
+//@Autowired → inject bean from context
